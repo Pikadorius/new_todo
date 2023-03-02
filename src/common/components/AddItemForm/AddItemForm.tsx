d1dd7f15656e6fdf7de4a0dd4495f6a3440b7373
Pikadorius <karpukea@yandex.by>
2023-03-02 17:10:16 +0300

    some check on empty value

--- src/common/components/AddItemForm/AddItemForm.tsx
+++ src/common/components/AddItemForm/AddItemForm.tsx
@@ -1,22 +1,28 @@
-import React, {createRef, FC } from 'react';
+import React, {createRef, FC, KeyboardEvent} from 'react';
 
 type PropsType = {
     callback: (value: string) => void
 }
 
-const AddItemForm:FC<PropsType> = ({callback}) => {
+const AddItemForm: FC<PropsType> = ({callback}) => {
     const inputRef = createRef<HTMLInputElement>()
 
     const addItem = () => {
-        if(inputRef.current && inputRef.current.value!=='') {
+        if (inputRef.current && inputRef.current.value !== '') {
             callback(inputRef.current.value)
-            inputRef.current.value=''
+            inputRef.current.value = ''
+        }
+    }
+
+    const onEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            addItem()
         }
     }
 
     return (
         <div>
-            <input type={'text'} ref={inputRef} />
+            <input type={'text'} ref={inputRef} onKeyDown={onEnterHandler}/>
             <button onClick={addItem}>Add item</button>
         </div>
     );
