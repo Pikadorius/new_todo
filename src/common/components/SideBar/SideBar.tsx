491863f9e33433c48bee07f9cef965b77265d04a
Pikadorius <karpukea@yandex.by>
2023-03-04 16:34:14 +0300

    added sidebar for todolists

--- src/common/components/SideBar/SideBar.tsx
+++ src/common/components/SideBar/SideBar.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import TodolistsList from 'features/todolists/TodolistsList/TodolistsList';
+import React, {useEffect} from 'react';
 import s from 'common/components/SideBar/SideBar.module.scss'
-import { NavLink} from 'react-router-dom';
-import {useAppSelector} from 'common/hooks/hooks';
-import {PATH} from 'common/constants/PATH';
+import {useAppDispatch, useAppSelector} from 'common/hooks/hooks';
+import Todolist from 'features/todolists/TodolistsList/Todolist/Todolist';
+import {setAppPage, setModalType} from 'app/appSlice';
+import addIcon from 'assets/icons/add.svg';
+import {fetchTodosTC} from 'features/todolists/todolistsSlice';
 
 const SideBar = () => {
+    const dispatch = useAppDispatch()
+
+
+    useEffect(() => {
+        dispatch(fetchTodosTC())
+        // dispatch(setAppPage('Todolist App'))
+    }, [])
+    const addTodolist = () => {
+        dispatch(setModalType('createTodo'))
+    }
+
     const todolists = useAppSelector(state => state.todolists)
     return (
         <div className={s.sidebar}>
-            {todolists.map(t=>{
-                return <NavLink key={t.id} to={`${PATH.TODOLISTS}/${t.id}`}>{t.title}</NavLink>
+            <h2 className={s.sidebarTitle}>
+                Todolists
+               <button className={s.noBtn} onClick={addTodolist}><img src={addIcon} alt={'add'}/></button>
+            </h2>
+            {todolists.map(t => {
+                return <Todolist key={t.id} {...t}/>
             })}
         </div>
     );
