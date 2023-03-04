08fa758ff1cb3f1377030fd97a31ef6f3a3f39c9
Pikadorius <karpukea@yandex.by>
2023-03-04 16:33:55 +0300

    added new Greeting component

---
+++ src/common/components/Greetings/Greetings.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import s from './Greetings.module.scss'
+import {useAppSelector} from 'common/hooks/hooks';
+
+const Greetings = () => {
+    const userName = useAppSelector(state => state.auth.user.login)
+
+    return (
+        <div className={s.container}>
+            <div className={s.wrapper}>
+                <div className={s.title}>
+                    <h2>Hello, {userName}!</h2>
+                    <h3>Welcome to my <a target={'_blank'} className={s.link} href={'https://github.com/Pikadorius/new_todo'} title={'Here you can see code'}>Todolist App</a></h3>
+                </div>
+                <div className={s.technologies}>
+                    <h4>Used technologies and features:</h4>
+                    <ul>
+                        <li>Typescript</li>
+                        <li>Redux Toolkit</li>
+                        <li>React router dom</li>
+                        <li>React hook form</li>
+                        <li>SCSS</li>
+                        <li>Protected routes</li>
+                        <li>Hooks</li>
+                        <li>Reusable components</li>
+                        <li>Local storage</li>
+                        <li>No libraries of React UI components {'\n'} (only custom components)</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Greetings;
