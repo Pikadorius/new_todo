33204b5ed11df980fbf06635ee908eb024325b1f
Pikadorius <karpukea@yandex.by>
2023-03-04 16:29:15 +0300

    added sidebar instead of page of todo

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {PATH} from "common/constants/PATH";
-import TodolistsList from "features/todolists/TodolistsList/TodolistsList";
 import RequireAuth from "pages/RequireAuth";
 import TasksList from "features/tasks/TasksList/TasksList";
 import Login from "features/auth/Login/Login";
+import SideBar from 'common/components/SideBar/SideBar';
+import Greetings from 'common/components/Greetings/Greetings';
+import {useAppSelector} from 'common/hooks/hooks';
 
 const Pages = () => {
-    return (
 
-        <Routes>
-            <Route path={PATH.LOGIN} element={<Login/>}/>
-            <Route element={<RequireAuth/>}>
-                <Route path={PATH.TASKS} element={<TasksList/>}/>
-                <Route path={PATH.TODOLISTS} element={<TodolistsList/>}/>
-                <Route path={'/'} element={<Navigate to={PATH.TODOLISTS}/>}/>
-            </Route>
-        </Routes>
+    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+
+    return (
+        <>
+            {isLoggedIn && <SideBar/>}
+            <Routes>
+                <Route path={PATH.LOGIN} element={<Login/>}/>
+                <Route element={<RequireAuth/>}>
+                    <Route path={PATH.TASKS} element={<TasksList/>}/>
+                    <Route path={PATH.MAIN} element={<Greetings/>}/>
+                    <Route path={'/'} element={<Navigate to={PATH.MAIN}/>}/>
+                </Route>
+            </Routes>
+        </>
     );
 };
 
