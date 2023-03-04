816788606f31e848beb8797d1be952d894f5d815
Pikadorius <karpukea@yandex.by>
2023-03-04 18:10:02 +0300

    test for update task

--- src/features/tasks/TasksList/Task/Task.tsx
+++ src/features/tasks/TasksList/Task/Task.tsx
@@ -1,8 +1,10 @@
 import React, {FC, useEffect} from 'react';
-import {TaskDomainType} from "features/tasks/tasksTypes";
+import {TaskDomainType, UpdateTaskType} from "features/tasks/tasksTypes";
 import {useAppDispatch, useAppSelector} from "common/hooks/hooks";
 import { setModalTask, setModalType} from 'app/appSlice';
 import s from './Task.module.scss'
+import {updateTodoTC} from 'features/todolists/todolistsSlice';
+import {updateTaskTC} from 'features/tasks/tasksSlice';
 
 const Task:FC<TaskDomainType> = (props) => {
     const dispatch = useAppDispatch()
@@ -11,11 +13,22 @@ const Task:FC<TaskDomainType> = (props) => {
         dispatch(setModalType('deleteTask'))
         dispatch(setModalTask(props))
     }
+
+    const newTask:UpdateTaskType = {
+        ...props,
+        status: 1,
+        title: 'lol'
+    }
+    const updateTask = () => {
+        dispatch(updateTaskTC({newTask, taskId: props.id, todoId: props.todoListId}))
+    }
+
     return (
         <div className={s.container}>
             <input type={'checkbox'} checked={props.status===1}/>
             <span>{props.title}</span>
             <button onClick={deleteHandler}>x</button>
+            <button onClick={updateTask}>update</button>
         </div>
     );
 };
