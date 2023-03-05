db24e8fc850859899534767f27156c6f752e5066
Pikadorius <karpukea@yandex.by>
2023-03-05 10:54:32 +0300

    refactor task component

--- src/features/tasks/TasksList/Task/Task.tsx
+++ src/features/tasks/TasksList/Task/Task.tsx
@@ -1,11 +1,22 @@
 import React, {FC} from 'react';
-import {TaskDomainType, UpdateTaskType} from "features/tasks/tasksTypes";
+import {TaskDomainType} from "features/tasks/tasksTypes";
 import {useAppDispatch} from "common/hooks/hooks";
 import { setModalTask, setModalType} from 'app/appSlice';
 import s from './Task.module.scss'
+import changeIcon from 'assets/icons/change.svg';
+import {NavLink} from 'react-router-dom';
+import {PATH} from 'common/constants/PATH';
+import deleteIcon from 'assets/icons/delete.svg';
 
-const Task:FC<TaskDomainType> = (props) => {
+type Props = {
+    taskStatus: 'active' | 'inProgress' | 'completed'
+}
+const Task:FC<TaskDomainType & Props> = (props) => {
     const dispatch = useAppDispatch()
+
+    const taskClass =
+        props.taskStatus === 'inProgress' ?
+            `${s.inProgress} ${s.container}` : props.taskStatus==='completed' ? `${s.container} ${s.completed}` : s.container
 
     const deleteHandler = () => {
         dispatch(setModalType('deleteTask'))
@@ -18,12 +29,13 @@ const Task:FC<TaskDomainType> = (props) => {
     }
 
     return (
-        <div className={s.container}>
-            <input type={'checkbox'} checked={props.status===1}/>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-            <button onClick={deleteHandler}>x</button>
-            <button onClick={updateTask}>update</button>
+        <div className={taskClass}>
+            <div className={s.taskHeader} >
+                <button onClick={updateTask} className={s.noBtn}><img src={changeIcon} alt={'Change'}/></button>
+                <h3 className={s.taskTitle}>{props.title}</h3>
+                <button onClick={deleteHandler} className={s.noBtn}><img src={deleteIcon} alt={'Change'}/></button>
+            </div>
+            {/*<p>{props.description}</p>*/}
         </div>
     );
 };
