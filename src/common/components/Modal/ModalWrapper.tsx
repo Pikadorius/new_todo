71b4146eca7c11ff371b5d9d88b4c160a6c334e1
Pikadorius <karpukea@yandex.by>
2023-03-08 13:51:21 +0300

    added close on click on background

--- src/common/components/Modal/ModalWrapper.tsx
+++ src/common/components/Modal/ModalWrapper.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Portal from 'common/components/Portal/Portal';
 import s from 'common/components/Modal/ModalWrapper.module.css'
 import ModalVariants from 'common/components/Modal/ModalVariants/ModalVariants';
+import {useAppDispatch} from 'common/hooks/hooks';
+import {setModalType} from 'app/appSlice';
 
 const ModalWrapper = () => {
+
+    const dispatch = useAppDispatch()
+
+    const closeHandler = () => {
+        dispatch(setModalType('idle'))
+    }
     return (
         <Portal>
-            <div className={s.container}>
-                <div className={s.wrapper}>
+            <div className={s.container} onClick={closeHandler}>
+                <div className={s.wrapper} onClick={(e) => e.stopPropagation()}>
                     <ModalVariants/>
                 </div>
             </div>
