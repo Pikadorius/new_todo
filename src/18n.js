348e13e4b24d2b3627f511a17e215528b62f4122
Pikadorius <karpukea@yandex.by>
2023-03-13 12:04:01 +0300

    fix loading translations from 'dev'

--- src/18n.js
+++ src/18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 i18n
     .use(Backend)
@@ -9,7 +9,8 @@ i18n
     .use(initReactI18next)
     .init({
         lng: 'en',
-        debug: true,
+        supportedLngs: ['en', 'ru'],
+        debug: false,
         backend: {
             loadPath: '/new_todo/locales/{{lng}}/{{ns}}.json'
         },
