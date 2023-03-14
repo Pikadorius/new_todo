0192fe3d96f20262e30df2292c7db7130d2be10e
Pikadorius <karpukea@yandex.by>
2023-03-14 14:40:34 +0300

    changed link hover color

--- src/common/components/Greetings/Greetings.tsx
+++ src/common/components/Greetings/Greetings.tsx
@@ -3,17 +3,19 @@ import s from './Greetings.module.scss'
 import {useAppSelector} from 'common/hooks/hooks';
 import {loggedUserSelector} from 'features/auth/authSelectors';
 import {useTranslation} from 'react-i18next';
+import {themeSelector} from 'features/theme/themeSelectors';
 
 const Greetings = () => {
     const userName = useAppSelector(loggedUserSelector).login
     const {t} = useTranslation()
+    const theme = useAppSelector(themeSelector)
 
     return (
         <div className={s.container}>
             <div className={s.wrapper}>
                 <div className={s.title}>
                     <h2>{t('greetings.hello')}, {userName}!</h2>
-                    <h3>{t('greetings.welcome')} <a  rel={'noreferrer'} target={'_blank'} className={s.link} href={'https://github.com/Pikadorius/new_todo'} title={t('greetings.code') || ''}>Todolist App</a></h3>
+                    <h3>{t('greetings.welcome')} <a  rel={'noreferrer'} target={'_blank'} className={theme==='dark' ? s.link : `${s.link} ${s.light}`} href={'https://github.com/Pikadorius/new_todo'} title={t('greetings.code') || ''}>Todolist App</a></h3>
                 </div>
                 <div className={s.technologies}>
                     <h4>{t('greetings.technologies')}</h4>
