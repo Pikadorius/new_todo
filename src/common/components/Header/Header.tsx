317cdb8c76d11f2b28bdd28164cc845e5b6ec219
Pikadorius <karpukea@yandex.by>
2023-03-17 13:02:37 +0300

    deleted all task adding logic; deleted show active todo title; deleted tooltip on hover;

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,21 +1,14 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import s from 'common/components/Header/Header.module.scss'
 import {useAppDispatch, useAppSelector} from "common/hooks/hooks";
 import {logoutTC} from 'features/auth/authSlice';
-import {setModalType} from 'app/appSlice';
-import addMain from 'assets/icons/addMain.svg'
-import addOrange from 'assets/icons/addOrange.svg'
-import {useLocation} from 'react-router-dom';
 import {isLoggedSelector, loggedUserSelector} from 'features/auth/authSelectors';
 import {useTranslation} from 'react-i18next';
-import Tooltip from 'common/components/Tooltip/Tooltip';
 import LanguageSelect from "common/components/LanguageSelect/LanguageSelect";
 import Theme from 'common/components/ThemeSelect/ThemeSelect';
 import {themeSelector} from 'features/theme/themeSelectors';
 
 const Header = () => {
-    const id = useLocation().pathname.slice(6)
-    const todo = useAppSelector(state => state.todolists.find(t => t.id === id))
     const isLoggedIn = useAppSelector(isLoggedSelector)
     const userName = useAppSelector(loggedUserSelector).login
     const theme = useAppSelector(themeSelector)
@@ -23,47 +16,14 @@ const Header = () => {
     const {t} = useTranslation()
     const dispatch = useAppDispatch()
 
-    const refSetTimeout = useRef<NodeJS.Timeout>();
-    const [tooltip, showTooltip] = useState(false)
-    const onMouseEnterHandler = () => {
-        refSetTimeout.current = setTimeout(() => {
-            showTooltip(true);
-        }, 500);
-    };
-
-    const onMouseLeaveHandler = () => {
-        clearTimeout(refSetTimeout.current);
-        showTooltip(false);
-    };
-
-
-    let page: string
-
-    if (id && todo) {
-        page = todo.title
-    } else {
-        page = 'Todolist App'
-    }
-
     const logout = () => {
         dispatch(logoutTC())
     }
 
-    const addTask = () => {
-        dispatch(setModalType('createTask'))
-    }
-
     return (
         <div className={theme==='dark' ? s.container : `${s.container} ${s.light}`}>
-            <div className={s.plug}></div>
             <h2 className={s.title}>
-                <div className={s.page} onMouseEnter={onMouseEnterHandler}
-                     onMouseLeave={onMouseLeaveHandler}>{page}</div>
-                {page !== 'Todolist App' && tooltip && <Tooltip text={page}><></>
-                </Tooltip>}
-                {(page !== 'Todolist App' && page !== 'Следи за делами') && isLoggedIn &&
-                    <button className={s.noBtn} title={'Add new task'} onClick={addTask}><img src={theme==='dark' ? addMain : addOrange}
-                                                                                              alt={'add'}/></button>}
+                ToDo App
             </h2>
             <div className={s.logoutField}>
                 <Theme/>
