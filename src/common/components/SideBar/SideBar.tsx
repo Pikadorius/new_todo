29a92c590b65ef3c0e71718abd33e81345c5ab0b
Pikadorius <karpukea@yandex.by>
2023-03-17 13:30:50 +0300

    some refactor

--- src/common/components/SideBar/SideBar.tsx
+++ src/common/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FC} from 'react';
 import s from 'common/components/SideBar/SideBar.module.scss'
 import {useAppDispatch, useAppSelector} from 'common/hooks/hooks';
 import Todolist from 'features/todolists/Todolist/Todolist';
@@ -9,28 +9,34 @@ import {TodolistDomainType} from 'features/todolists/todolistsTypes';
 import {useTranslation} from 'react-i18next';
 import {themeSelector} from 'features/theme/themeSelectors';
 
-const SideBar = (props: { todolists: TodolistDomainType[], isShowed: boolean, setShowed: (value: boolean) => void }) => {
+type PropsType = {
+    todolists: TodolistDomainType[]
+    isShowed: boolean
+    setShowed: (value: boolean) => void
+}
+const SideBar: FC<PropsType> = ({todolists, isShowed, setShowed}) => {
     const dispatch = useAppDispatch()
     const theme = useAppSelector(themeSelector)
+    const height = window.innerHeight - 60
 
     const {t} = useTranslation('translation');
     const addTodolist = () => {
         dispatch(setModalType('createTodo'))
     }
 
-    const height = window.innerHeight-60
-
-    const sidebar = theme === 'dark' ? s.sidebar : `${s.sidebar} ${s.light}`
+    const sidebarClass = theme === 'dark' ? s.sidebar : `${s.sidebar} ${s.light}`
 
     return (
-        <div style={{height}} className={props.isShowed ? sidebar : `${sidebar} ${s.closed}`}>
+        <div style={{height}} className={isShowed ? sidebarClass : `${sidebarClass} ${s.closed}`}>
             <h2 className={s.sidebarTitle}>
                 <div>{t('todolists.title')}</div>
-                <button className={s.noBtn} onClick={addTodolist}><img src={theme === 'dark' ? addIcon : addBlack}
-                                                                       alt={'add'}/></button>
+                <button className={s.noBtn} onClick={addTodolist}>
+                    <img src={theme === 'dark' ? addIcon : addBlack}
+                         alt={'add'}/>
+                </button>
             </h2>
-            {props.todolists.map(t => {
-                return <Todolist key={t.id} {...t} setSidebar={props.setShowed}/>
+            {todolists.map(t => {
+                return <Todolist key={t.id} {...t} setSidebar={setShowed}/>
             })}
         </div>
     );
