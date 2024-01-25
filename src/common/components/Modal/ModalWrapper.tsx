a1496ac515758f140b0e627d8272a3132770aa28
Pikadorius <karpukea@yandex.by>
2024-01-25 22:17:21 +0300

    antd test

--- src/common/components/Modal/ModalWrapper.tsx
+++ src/common/components/Modal/ModalWrapper.tsx
@@ -1,42 +1,51 @@
-import React, {KeyboardEvent, useEffect} from 'react';
-import Portal from 'common/components/Portal/Portal';
-import s from 'common/components/Modal/ModalWrapper.module.css'
-import ModalVariants from 'common/components/Modal/ModalVariants/ModalVariants';
-import {useAppDispatch, useAppSelector} from 'common/hooks/hooks';
-import {setModalType} from 'app/appSlice';
-import {modalTypeSelector} from 'app/appSelectors';
+import React, { useEffect } from "react";
+import ModalVariants from "common/components/Modal/ModalVariants/ModalVariants";
+import { useAppDispatch, useAppSelector } from "common/hooks/hooks";
+import { setModalType } from "app/appSlice";
+import { modalTypeSelector } from "app/appSelectors";
+import { Modal } from "antd";
 
 const ModalWrapper = () => {
-    const modal = useAppSelector(modalTypeSelector) === 'idle'
-    const dispatch = useAppDispatch()
+  const modal = useAppSelector(modalTypeSelector) !== "idle";
+  const dispatch = useAppDispatch();
 
-    let listener = (e: any) => {
-        if (e.key === 'Escape' && !modal) {
-            closeHandler()
-        }
+  let listener = (e: any) => {
+    if (e.key === "Escape" && !modal) {
+      closeHandler();
     }
+  };
 
-    useEffect(()=>{
-        document.addEventListener('keydown', listener)
-        return ()=>{
-            document.removeEventListener('keydown', listener)
-        }
-    },[])
+  useEffect(() => {
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
 
-    const closeHandler = () => {
-        dispatch(setModalType('idle'))
-        document.removeEventListener('keydown', listener)
-    }
+  const closeHandler = () => {
+    dispatch(setModalType("idle"));
+    document.removeEventListener("keydown", listener);
+  };
 
-    return (
-        <Portal>
-            <div className={s.container} onClick={closeHandler} id={'modal'}>
-                <div className={s.wrapper} onClick={(e) => e.stopPropagation()}>
-                    <ModalVariants/>
-                </div>
-            </div>
-        </Portal>
-    );
+  return (
+    // <Portal>
+    //   <div className={s.container} onClick={closeHandler} id={"modal"}>
+    //     <div className={s.wrapper} onClick={(e) => e.stopPropagation()}>
+    //       <ModalVariants />
+    //     </div>
+    //   </div>
+    // </Portal>
+    <Modal
+      open={modal}
+      destroyOnClose
+      onCancel={closeHandler}
+      footer={false}
+      closeIcon={false}
+      style={{ padding: "0" }}
+    >
+      <ModalVariants />
+    </Modal>
+  );
 };
 
-export default ModalWrapper
+export default ModalWrapper;
