2d815d3dc081b2256e3ee496cad23469479d4953
Pikadorius <karpukea@yandex.by>
2024-01-25 20:46:39 +0300

    api key

--- src/common/constants/instance.ts
+++ src/common/constants/instance.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1',
-    withCredentials: true,
-    headers: {
-        'API-KEY': 'abc137fc-ad0c-49be-975b-e12bdb8a93ad'
-    }
-})
+  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  withCredentials: true,
+  headers: {
+    "API-KEY": "8a37ea32-6abd-4e89-bdeb-bec7e96dbd2f",
+  },
+});
 
-export type DefaultResponseType<T={}> = {
-    data: T,
-    resultCode: number,
-    messages: string[],
-}
+export type DefaultResponseType<T = {}> = {
+  data: T;
+  resultCode: number;
+  messages: string[];
+};
