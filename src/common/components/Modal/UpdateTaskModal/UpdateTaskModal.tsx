6124688dbfd63252e6d14b1a24368fb78a36ddd2
Pikadorius <karpukea@yandex.by>
2026-01-03 12:54:23 +0300

    some changes

--- src/common/components/Modal/UpdateTaskModal/UpdateTaskModal.tsx
+++ src/common/components/Modal/UpdateTaskModal/UpdateTaskModal.tsx
@@ -1,62 +1,81 @@
-import React from 'react';
-import ModalHeader from 'common/components/Modal/components/ModalHeader/ModalHeader';
-import {useAppDispatch, useAppSelector} from 'common/hooks/hooks';
-import ModalFooter from 'common/components/Modal/components/ModalFooter/ModalFooter';
-import {setModalType} from 'app/appSlice';
-import {updateTaskTC} from 'features/tasks/tasksSlice';
-import s from './UpdateTaskModal.module.scss'
-import common from 'common/components/Modal/CommonModals.module.scss'
-import {SubmitHandler, useForm} from 'react-hook-form';
-import {UpdateTaskType} from 'features/tasks/tasksTypes';
-import {useTranslation} from 'react-i18next';
+import React from "react";
+import ModalHeader from "common/components/Modal/components/ModalHeader/ModalHeader";
+import { useAppDispatch, useAppSelector } from "common/hooks/hooks";
+import ModalFooter from "common/components/Modal/components/ModalFooter/ModalFooter";
+import { setModalType } from "app/appSlice";
+import { updateTaskTC } from "features/tasks/tasksSlice";
+import s from "./UpdateTaskModal.module.scss";
+import common from "common/components/Modal/CommonModals.module.scss";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { UpdateTaskType } from "features/tasks/tasksTypes";
+import { useTranslation } from "react-i18next";
 
 const UpdateTaskModal = () => {
-    const dispatch = useAppDispatch()
-    const task = useAppSelector(state => state.app.modalTask)
-    const {t} = useTranslation()
+  const dispatch = useAppDispatch();
+  const task = useAppSelector((state) => state.app.modalTask);
+  const { t } = useTranslation();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Partial<UpdateTaskType>>({
+    mode: "onTouched",
+    defaultValues: {
+      title: task.title,
+      description: task.description,
+      status: task.status,
+    },
+  });
+  const onSubmit: SubmitHandler<Partial<UpdateTaskType>> = (data) => {
+    dispatch(
+      updateTaskTC({ todoId: task.todoListId, taskId: task.id, newTask: data })
+    ).then(() => {
+      dispatch(setModalType("idle"));
+    });
+  };
 
-    const {register, handleSubmit, formState: {errors}} = useForm<Partial<UpdateTaskType>>({
-        mode: 'onTouched',
-        defaultValues: {
-            title: task.title,
-            description: task.description,
-            status: task.status
-        }
-    },);
-    const onSubmit: SubmitHandler<Partial<UpdateTaskType>> = data => {
-        dispatch(updateTaskTC({todoId: task.todoListId, taskId: task.id, newTask: data})).then(() => {
-            dispatch(setModalType('idle'))
-        })
-    }
-
-    return (
-        <>
-            <ModalHeader title={t("modal.update_task.header")}/>
-            <div className={common.modalBody}>
-                <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-                    <div className={s.field}>
-                        <div className={s.fieldName}>{t("modal.update_task.title")}</div>
-                        <input autoFocus type={'text'} {...register("title", {required: true, min: 1})} className={s.input} placeholder={errors.title?.message}/>
-                        {errors.title && <span className={s.errorField}>{t("modal.required")}</span>}
-                    </div>
-                    <div className={s.field}>
-                        <div className={s.fieldName}>{t("modal.update_task.description")}</div>
-                        <textarea {...register("description")} className={s.textarea}/>
-                    </div>
-                    <div className={s.field}>
-                        <div className={s.fieldName}>{t("modal.update_task.status")}</div>
-                        <select {...register("status")} className={s.select}>
-                            <option value={0}>{t("modal.active")}</option>
-                            <option value={1}>{t("modal.in_progress")}</option>
-                            <option value={2}>{t("modal.completed")}</option>
-                        </select>
-                    </div>
-                </form>
+  return (
+    <>
+      <ModalHeader title={t("modal.update_task.header")} />
+      <div className={common.modalBody}>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+          <div className={s.field}>
+            <div className={s.fieldName}>{t("modal.update_task.title")}</div>
+            <input
+              autoFocus
+              type={"text"}
+              {...register("title", { required: true, min: 1 })}
+              className={s.input}
+              placeholder={errors.title?.message}
+            />
+            {errors.title && (
+              <span className={s.errorField}>{t("modal.required")}</span>
+            )}
+          </div>
+          <div className={s.field}>
+            <div className={s.fieldName}>
+              {t("modal.update_task.description")}
             </div>
-            <ModalFooter type={'todo'} title={t("modal.update_btn")} callback={handleSubmit(onSubmit)}/>
-        </>
-    );
+            <textarea {...register("description")} className={s.textarea} />
+          </div>
+          <div className={s.field}>
+            <div className={s.fieldName}>{t("modal.update_task.status")}</div>
+            <select {...register("status")} className={s.select}>
+              <option value={0}>{t("modal.active")}</option>
+              <option value={1}>{t("modal.in_progress")}</option>
+              <option value={2}>{t("modal.completed")}</option>
+            </select>
+          </div>
+        </form>
+      </div>
+      <ModalFooter
+        type={"todo"}
+        title={t("modal.update_btn")}
+        callback={handleSubmit(onSubmit)}
+      />
+    </>
+  );
 };
 
 export default UpdateTaskModal;
--- src/features/todolists/todolistsSlice.ts
+++ src/features/todolists/todolistsSlice.ts
@@ -1,132 +1,177 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {TodolistDomainType, TodolistResponseType} from "features/todolists/todolistsTypes";
-import {todolistsAPI} from "features/todolists/todolistsAPI";
-import {setAppError, setAppStatus} from "app/appSlice";
-import {tasksAPI} from 'features/tasks/tasksAPI';
-import {TaskType} from 'features/tasks/tasksTypes';
-import i18next from 'i18next';
-import {errorHandler} from "common/utils/errorHandlers";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  TodolistDomainType,
+  TodolistResponseType,
+} from "features/todolists/todolistsTypes";
+import { todolistsAPI } from "features/todolists/todolistsAPI";
+import { setAppError, setAppStatus } from "app/appSlice";
+import { tasksAPI } from "features/tasks/tasksAPI";
+import { TaskType } from "features/tasks/tasksTypes";
+import i18next from "i18next";
+import { errorHandler } from "common/utils/errorHandlers";
 
-const {t} = i18next
-const initialState = [] as TodolistDomainType[]
+const { t } = i18next;
+const initialState = [] as TodolistDomainType[];
 
-export const fetchTodosTC = createAsyncThunk('fetchTodolists', async (_, {dispatch}) => {
-    dispatch(setAppStatus('loading'))
+export const fetchTodosTC = createAsyncThunk(
+  "fetchTodolists",
+  async (_, { dispatch }) => {
+    dispatch(setAppStatus("loading"));
     try {
-        const res = await todolistsAPI.fetchTodos()
-        dispatch(setTodolists(res.data))
-        if (res.data.length) {
-            res.data.forEach(t => dispatch(fetchTodosTasksTC(t.id)))
-        }
-        dispatch(setAppStatus('success'))
+      const res = await todolistsAPI.fetchTodos();
+      dispatch(setTodolists(res.data));
+      if (res.data.length) {
+        res.data.forEach((t) => dispatch(fetchTodosTasksTC(t.id)));
+      }
+      dispatch(setAppStatus("success"));
     } catch (e: any) {
-        errorHandler(e, dispatch)
+      errorHandler(e, dispatch);
     } finally {
-        dispatch(setAppStatus('idle'))
+      dispatch(setAppStatus("idle"));
     }
+  }
+);
 
-})
-
-export const fetchTodosTasksTC = createAsyncThunk('fetchTodolists', async (id: string, {dispatch}) => {
-    dispatch(setAppStatus('loading'))
+export const fetchTodosTasksTC = createAsyncThunk(
+  "fetchTodolists",
+  async (id: string, { dispatch }) => {
+    dispatch(setAppStatus("loading"));
     try {
-        const res = await tasksAPI.fetchTasks(id)
-        dispatch(setTodosTasks({id, tasksCount: res.data.totalCount, tasks: res.data.items}))
-        dispatch(setAppStatus('success'))
+      const res = await tasksAPI.fetchTasks(id);
+      dispatch(
+        setTodosTasks({
+          id,
+          tasksCount: res.data.totalCount,
+          tasks: res.data.items,
+        })
+      );
+      dispatch(setAppStatus("success"));
     } catch (e: any) {
-        errorHandler(e, dispatch)
+      errorHandler(e, dispatch);
     } finally {
-        dispatch(setAppStatus('idle'))
+      dispatch(setAppStatus("idle"));
     }
-})
+  }
+);
 
-export const createTodoTC = createAsyncThunk('createTodo', async (title: string, {dispatch}) => {
-        dispatch(setAppStatus('loading'))
-        try {
-            const res = await todolistsAPI.createTodo(title)
-            if (res.data.resultCode === 0) {
-                dispatch(addTodo({...res.data.data.item, tasksCount: 0, tasks: []}))
-                dispatch(setAppStatus('success'))
-                dispatch(setAppError(t("popUp.create_todo")))
-            } else {
-                dispatch(setAppStatus('failed'))
-                dispatch(setAppError(res.data.messages[0]))
-            }
-        } catch (e: any) {
-            errorHandler(e, dispatch)
-        } finally {
-            dispatch(setAppStatus('idle'))
-        }
-
-    }
-)
-
-export const deleteTodoTC = createAsyncThunk('deleteTodo', async (todoId: string, {dispatch}) => {
-    dispatch(setAppStatus('loading'))
+export const createTodoTC = createAsyncThunk(
+  "createTodo",
+  async (title: string, { dispatch }) => {
+    dispatch(setAppStatus("loading"));
     try {
-        const res = await todolistsAPI.deleteTodo(todoId)
-        if (res.data.resultCode === 0) {
-            dispatch(deleteTodo(todoId))
-            dispatch(setAppError(t("popUp.delete_todo")))
-            dispatch(setAppStatus('success'))
-        } else {
-            dispatch(setAppStatus('failed'))
-            dispatch(setAppError(res.data.messages[0]))
-        }
+      const res = await todolistsAPI.createTodo(title);
+      if (res.data.resultCode === 0) {
+        dispatch(addTodo({ ...res.data.data.item, tasksCount: 0, tasks: [] }));
+        dispatch(setAppStatus("success"));
+        dispatch(setAppError(t("popUp.create_todo")));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError(res.data.messages[0]));
+      }
     } catch (e: any) {
-        errorHandler(e, dispatch)
+      errorHandler(e, dispatch);
     } finally {
-        dispatch(setAppStatus('idle'))
+      dispatch(setAppStatus("idle"));
     }
-})
+  }
+);
 
-export const updateTodoTC = createAsyncThunk('updateTodo', async (data: { todoId: string, title: string }, {dispatch}) => {
-    dispatch(setAppStatus('loading'))
+export const deleteTodoTC = createAsyncThunk(
+  "deleteTodo",
+  async (todoId: string, { dispatch }) => {
+    dispatch(setAppStatus("loading"));
     try {
-        const res = await todolistsAPI.updateTodo(data.todoId, data.title)
-        if (res.data.resultCode === 0) {
-            dispatch(updateTodoTitle(data))
-            dispatch(setAppStatus('success'))
-            dispatch(setAppError(t("popUp.update_todo")))
-        } else {
-            dispatch(setAppStatus('failed'))
-            dispatch(setAppError(res.data.messages[0]))
-        }
+      const res = await todolistsAPI.deleteTodo(todoId);
+      if (res.data.resultCode === 0) {
+        dispatch(deleteTodo(todoId));
+        dispatch(setAppError(t("popUp.delete_todo")));
+        dispatch(setAppStatus("success"));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError(res.data.messages[0]));
+      }
     } catch (e: any) {
-        errorHandler(e, dispatch)
+      errorHandler(e, dispatch);
     } finally {
-        dispatch(setAppStatus('idle'))
+      dispatch(setAppStatus("idle"));
     }
+  }
+);
 
-})
+export const updateTodoTC = createAsyncThunk(
+  "updateTodo",
+  async (data: { todoId: string; title: string }, { dispatch }) => {
+    dispatch(setAppStatus("loading"));
+    try {
+      const res = await todolistsAPI.updateTodo(data.todoId, data.title);
+      if (res.data.resultCode === 0) {
+        dispatch(updateTodoTitle(data));
+        dispatch(setAppStatus("success"));
+        // dispatch(setAppError(t("popUp.update_todo")))
+        dispatch(setAppError(`${data.title} ${t("popUp.update_todo")}`));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError(res.data.messages[0]));
+      }
+    } catch (e: any) {
+      errorHandler(e, dispatch);
+    } finally {
+      dispatch(setAppStatus("idle"));
+    }
+  }
+);
 
 const todolistsSlice = createSlice({
-    name: 'todolists',
-    initialState,
-    reducers: {
-        setTodolists: (state, action: PayloadAction<TodolistResponseType[]>) => {
-            return action.payload.map(t => ({...t, tasksCount: 0, tasks: []}))
-        },
-        addTodo: (state, action: PayloadAction<TodolistDomainType>) => {
-            state.unshift(action.payload)
-        },
-        deleteTodo: (state, action: PayloadAction<string>) => {
-            const index = state.findIndex(t => t.id === action.payload)
-            state.splice(index, 1)
-        },
-        updateTodoTitle: (state, action: PayloadAction<{ todoId: string, title: string }>) => {
-            return state.map(t => t.id === action.payload.todoId ? {...t, title: action.payload.title} : t)
-        },
-        setTodosTasks: (state, action: PayloadAction<{ id: string, tasksCount: number, tasks: TaskType[] }>) => {
-            return state.map(t => t.id === action.payload.id ? {
-                ...t,
-                tasksCount: action.payload.tasksCount,
-                tasks: action.payload.tasks
-            } : t)
-        }
-    }
-})
+  name: "todolists",
+  initialState,
+  reducers: {
+    setTodolists: (state, action: PayloadAction<TodolistResponseType[]>) => {
+      return action.payload.map((t) => ({ ...t, tasksCount: 0, tasks: [] }));
+    },
+    addTodo: (state, action: PayloadAction<TodolistDomainType>) => {
+      state.unshift(action.payload);
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      const index = state.findIndex((t) => t.id === action.payload);
+      state.splice(index, 1);
+    },
+    updateTodoTitle: (
+      state,
+      action: PayloadAction<{ todoId: string; title: string }>
+    ) => {
+      return state.map((t) =>
+        t.id === action.payload.todoId
+          ? { ...t, title: action.payload.title }
+          : t
+      );
+    },
+    setTodosTasks: (
+      state,
+      action: PayloadAction<{
+        id: string;
+        tasksCount: number;
+        tasks: TaskType[];
+      }>
+    ) => {
+      return state.map((t) =>
+        t.id === action.payload.id
+          ? {
+              ...t,
+              tasksCount: action.payload.tasksCount,
+              tasks: action.payload.tasks,
+            }
+          : t
+      );
+    },
+  },
+});
 
-export const {setTodolists, addTodo, deleteTodo, updateTodoTitle, setTodosTasks} = todolistsSlice.actions
+export const {
+  setTodolists,
+  addTodo,
+  deleteTodo,
+  updateTodoTitle,
+  setTodosTasks,
+} = todolistsSlice.actions;
 
-export const todolistsReducer = todolistsSlice.reducer
+export const todolistsReducer = todolistsSlice.reducer;
