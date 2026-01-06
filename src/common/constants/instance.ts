abee6c2f020ed882c7d8d47100eb25bf5928e065
Pikadorius <karpukea@yandex.by>
2026-01-06 15:42:38 +0300

    some changes

--- src/common/constants/instance.ts
+++ src/common/constants/instance.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1",
   withCredentials: true,
   headers: {
-    "API-KEY": "8a37ea32-6abd-4e89-bdeb-bec7e96dbd2f",
+    "API-KEY": "912ee5e8-eb5b-4c5a-9077-a86e206013de",
   },
 });
 
